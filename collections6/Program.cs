c8cbbec6e6652d7c81f8953ffa5ae1b1c2271b4c
shaik hanifa <hanifashaik285@gmail.com>
2024-10-17 17:19:11 +0530

    Add project files.

---
+++ collections6/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace collections6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var college = new College("VTU University");
+            college.AddDepartment("Computer Science");
+            college.AddDepartment("Mathematics");
+            college.DisplayDepartments();
+            college.RemoveDepartment("Mathematics");
+            college.DisplayDepartments();
+            Console.ReadLine();
+        }
+
+    }
+}
+public class College
+{
+    public string Name
+    {
+        get;
+        set;
+    }
+    private List<string> Departments
+    {
+        get;
+        set;
+    }
+
+    public College(string name)
+    {
+        Name = name;
+        Departments = new List<string>();
+    }
+    public void AddDepartment(string departmentName)
+    {
+        Departments.Add(departmentName);
+    }
+    public void RemoveDepartment(string departmentName)
+    {
+        Departments.Remove(departmentName);
+    }
+    public void DisplayDepartments()
+    {
+        Console.WriteLine("Departments in (VTU University):");
+        foreach (var department in Departments)
+        {
+            Console.WriteLine(department);
+        }
+    }
+}
+
+       
